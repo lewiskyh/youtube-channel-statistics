f5f02577911e012b0ff187a52a9504b62028efde
Lewis K <lewis.kwok.2023@bristol.ac.uk>
2024-06-17 12:10:01 +0100

    fix web format

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from .utils import create_dataframe, save_data_plot
+from .utils import create_dataframe, save_data_plot, get_channel_details
 
 bp = Blueprint('main', __name__)
 
@@ -9,8 +9,11 @@ def index():
     if request.method == 'POST':
         channel_id = request.form['channel_id']
         data = create_dataframe(channel_id)
+        channel_details = get_channel_details(channel_id)
         save_data_plot(data, plot_type='views')
         save_data_plot(data, plot_type='likes')
-        return render_template("dashboard.html", data=data.to_html(classes='data', header=True, index=False))
+        return render_template("dashboard.html",
+                               channel_details=channel_details,
+                               data=data.to_html(classes='data', header=True, index=False))
 
-    return render_template("dashboard.html", data=None)
+    return render_template("dashboard.html", channel_details=None,data=None)
--- app/utils.py
+++ app/utils.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 import pandas as pd
 from datetime import datetime
 from matplotlib import pyplot as plt
-##Import Hiragino sans to display Japanese/Korean characters
+##Import Hiragino sans to display Japanese characters
 import matplotlib
-matplotlib.rcParams['font.family'] = 'Hiragino sans'
 
+matplotlib.rcParams['font.family'] = 'Hiragino sans'
 
 MAX_TITLE_LENGTH = 30
 SPACE_FOR_DOTS = 3
@@ -20,9 +20,9 @@ youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=API_KEY)
 if not API_KEY:
     raise ValueError("No API key found. Please set the YOUTUBE_API_KEY environment variable in .env file.")
 
+
 ##function to retrieve all video IDs
 def get_video_ids(channel_id):
-
     video_ids = []
     try:
         request = youtube.search().list(
@@ -55,7 +55,30 @@ def get_video_ids(channel_id):
     return video_ids
 
 
-##function to retrieve video statistics
+#Function to fetch channek details (e.g. channel name, subscribers and total view counts)
+def get_channel_details(channel_id):
+    channel_details = {
+        "channel_name": "",
+        "total_subscribers": "",
+        "total_videos": "",
+        "total_views": "",
+        "channel_description": ""
+    }
+    try:
+        request = youtube.channels().list(part="snippet,statistics", id=channel_id)
+        response = request.execute()
+        if response["items"]:
+            channel_details["channel_name"] = response["items"][0]["snippet"]["title"]
+            channel_details["total_subscribers"] = response["items"][0]["statistics"]["subscriberCount"]
+            channel_details["total_videos"] = response["items"][0]["statistics"]["videoCount"]
+            channel_details["total_views"] = response["items"][0]["statistics"]["viewCount"]
+            channel_details["channel_description"] = response["items"][0]["snippet"]["description"]
+    except HTTPError as error:
+        print(f"{error} has occurred")
+    return channel_details
+
+
+#function to retrieve video statistics
 def get_video_stats(video_id):
     try:
         request = youtube.videos().list(part="snippet,statistics", id=video_id)
@@ -93,27 +116,27 @@ def trim_title(track_name):
 
 def save_data_plot(df, plot_type="views"):
     if plot_type == "views":
-        top20_df = df.sort_values(by=['Views'], ascending=False)[:20]
-        top20_df["Title"] = top20_df["Title"].apply(trim_title)
+        top10_df = df.sort_values(by=['Views'], ascending=False)[:10]
+        top10_df["Title"] = top10_df["Title"].apply(trim_title)
         plt.figure(figsize=(12, 5))
-        plt.barh(top20_df['Title'], top20_df["Views"], color="green")
+        plt.barh(top10_df['Title'], top10_df["Views"], color="green")
         plt.xlabel("Views")
         plt.ylabel("Video Title")
-        plt.title("Top20 most viewed videos")
+        plt.title("Top10 most viewed videos")
+        plt.tight_layout()
         plt.gca().invert_yaxis()
-        plot_filename = os.path.join("app", "static", "view_plot.png")
+        plot_filename = os.path.join("app", "static", "views_plot.png")
     elif plot_type == "likes":
-        top20_df = df.sort_values(by=['Likes'], ascending=False)[:20]
-        top20_df["Title"] = top20_df["Title"].apply(trim_title)
+        top10_df = df.sort_values(by=['Likes'], ascending=False)[:10]
+        top10_df["Title"] = top10_df["Title"].apply(trim_title)
         plt.figure(figsize=(12, 5))
-        plt.barh(top20_df['Title'], top20_df["Likes"], color="blue")
+        plt.barh(top10_df['Title'], top10_df["Likes"], color="blue")
         plt.xlabel("Likes")
         plt.ylabel("Video Title")
-        plt.title("Top20 most liked videos")
+        plt.title("Top10 most liked videos")
+        plt.tight_layout()
         plt.gca().invert_yaxis()
-        plot_filename = os.path.join("app", "static", "like_plot.png")
+        plot_filename = os.path.join("app", "static", "likes_plot.png")
 
     plt.savefig(plot_filename)
     plt.close()
-
-
